c20336554ff9eb1b7b6d97995ddeef3beb54c02e
Khalil Hajlaoui <98230912+KhajlaouiBdeB@users.noreply.github.com>
2026-06-23 15:43:23 -0400

    [Code] Add Node classe

---
+++ Source/Game/BehaviourTree/Core/Node.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace BehaviourTree
+{
+    enum class NodeState
+    {
+        SUCCESS,
+        FAILURE,
+        RUNNING
+    };
+
+    class Node
+    {
+    public:
+        virtual ~Node() = default;
+        virtual NodeState tick() = 0;
+
+    protected:
+        Node() = default;
+    };
+}
