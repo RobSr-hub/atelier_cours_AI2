2c632eec40aa6d8cac2341babdced4f43f6aa38d
RobbieSR <robbiesutherlandrusso@gmail.com>
2026-08-11 14:58:21 -0400

    [Game] Add PlayerBot

---
+++ Source/Game/PlayerBot.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Vector2D.h"
+
+class Raven_Bot;
+class Raven_Scene;
+
+namespace Game
+{
+	class PlayerBot
+	{
+	public:
+		PlayerBot(Raven_Scene& scene, const Vector2D);
+		void HandleInput();
+		void Update();
+		void render() const;
+	
+	private:
+		Raven_Bot* _bot;
+	};
+
+}
